7e49e1fbddef4a16bb1a22421c6af8879ed34622
TGIWIT <122895427+TGIWIT@users.noreply.github.com>
2023-07-29 21:13:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 name = input("What's your name ? ")
 
-while not name == "Djamil":
+while not name == "Mozart":
     print("Error : You have to put the good name")
     name = input("What's your name ? ")
 
 
 age = input("How old are you ? ")
 
-while not age == "17":
+while not age == "23":
     print("Error : You have to put the good age") 
     age = input("How old are you ? ")  
 else:
@@ -16,21 +16,8 @@ else:
 
 password = input("Whats your password ? ")
 
-while not password == "Mboyaho":
+while not password == "Mbouzi":
     print("Error : please take the good password")
     password = input("Whats your password ? ")
 else:
     print("Password correct you have access to your account")
-
-
-# ----------------------------------------------------------------------------------------------------------------------
-                                    # MANIPULATION DES VALEURES NUMERIQUES
-"""n= 0
-
-print("debut de la boucle")
-
-while n < 10:
-    print("Valeur de n : " + str(n))
-    n = n + 1
-
-print("fin de la boucle")"""
